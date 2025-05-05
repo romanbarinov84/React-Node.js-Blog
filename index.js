90b5f6e7e416306892c0225489a39466e2782529
Roman <roman-meinl@ukr.net>
2025-05-05 17:23:48 -0200

    second commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+
+
+
+app.listen(4444, (err) => {
+    if(err){
+        return console.log(err);
+    }
+    console.log("server OK");
+    
+} )
