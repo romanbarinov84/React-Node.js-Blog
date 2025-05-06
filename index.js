afb4e2938a45ba494f241106b3477cd018282678
Roman <roman-meinl@ukr.net>
2025-05-06 12:54:06 -0200

    connect MongoDB

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb+srv://romanmeinl16:romanmeinl16@cluster0.uel1akm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB error", err));
+  
+
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.post("/auth/login", (req, res) => {
+  console.log(req.body);
 
+ if(req.body.email === "test@test.ua"){
+   }
+  const token = jwt.sign({
+    email: req.body.email,
+    fullName: "Roman Barinov",
+  }, "secret123",
+);
+
+  
+  res.json({
+    success:true,
+    token,
+  })
+});
 
 
 app.listen(4444, (err) => {
